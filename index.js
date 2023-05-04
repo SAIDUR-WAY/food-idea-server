de05454a078e20de836f26f497b0fb947305f394
saidur-way <saiduronway@gmail.com>
2023-05-04 13:06:55 +0600

    italian recipe data add and update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.port || 5000;
 
 const chefs = require('./data/italianChefs.json')
+const recipes = require('./data/italianRecipe.json')
 
 app.use(cors())
 
@@ -20,6 +21,10 @@ app.get('/chefs/:id',(req, res)=>{
      console.log(id)
      const chefdetails = chefs.find(chef => chef.id === id)
      res.send(chefdetails)
+})
+
+app.get('/recipes', (req, res)=>{
+     res.send(recipes)
 })
 
 app.listen(port, ()=>{
